20956490993d3977f33b99ed5b148ace8ceda318
Ricardo Da Silva <ricardo.dasilva@paydock.com>
2024-07-16 14:44:54 +0200

    Updated github publishing url

--- convention-plugins/src/main/kotlin/github-publish-convention.gradle.kts
+++ convention-plugins/src/main/kotlin/github-publish-convention.gradle.kts
@@ -24,11 +24,11 @@ if (secretPropsFile.exists()) {
 publishing {
     repositories {
         maven {
-            // https://github.com/PayDock/android-mobile-sdk
+            // https://github.com/PayDock/android-core-networking
             name = "GitHubPackages"
             // https://maven.pkg.github.com/{repository owner}/{repository}
             // url = uri("https://maven.pkg.github.com/GITHUB_USERID/REPOSITORY")
-            url = uri("https://maven.pkg.github.com/Paydock/android-mobile-sdk")
+            url = uri("https://maven.pkg.github.com/Paydock/android-core-networking")
             // Private Access Token - linked to a specific account (Paste token as-is, or define an environment variable to hold the token)
             credentials {
                 username = getExtraString("username")
