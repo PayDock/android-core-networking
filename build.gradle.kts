77a0d74e84f0dd378f39108fee3c7f88fa1b843f
Paolo Ricardo Da Silva <ricardo.dasilva@paydock.com>
2025-02-21 13:51:10 +0000

    SDK-1206 - [Android] Update all dependencies we use including gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
-    //trick: for the same plugin versions in all sub-modules
-    alias(libs.plugins.android.library).apply(false)
-    alias(libs.plugins.kotlin.multiplatform).apply(false)
-    alias(libs.plugins.kotlin.cocoapods).apply(false)
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.kotlin.cocoapods) apply false
 }
 
 tasks.register("clean").configure {
